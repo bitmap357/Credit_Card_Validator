d6d6e3ae4b305fe966fb3b92c0f1bd8223ee97da
bitmap357 <edakoyeram@gmail.com>
2023-07-07 17:59:03 +0000

    getting the even numbers and doubling them

--- Validator App.py
+++ Validator App.py
@@ -1,9 +1,11 @@
 card_no = "5610591081018250"
 odd_sum = 0
+double_list = []
 number = list(card_no)
 for (idx, val) in enumerate(number):
     if idx % 2 != 0:
         odd_sum += int(val)
+    else:
+        double_list.append(int(val)*2)
 
-print()
-
+# converting the list into a string
